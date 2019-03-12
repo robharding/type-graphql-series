6d23ca8a9820e138ea5c0d5c61fec535e720fddd
Rob Harding <robbie.harding@hotmail.com>
2019-03-12 13:48:23 -0400

    Switched the shared PasswordInput to a mixin

--- src/modules/shared/PasswordInput.ts
+++ src/modules/shared/PasswordInput.ts
@@ -1,9 +1,13 @@
-import { InputType, Field } from "type-graphql";
+import { InputType, Field, ClassType } from "type-graphql";
 import { Min } from "class-validator";
 
-@InputType()
-export class PasswordInput {
-	@Field()
-	@Min(5)
-	password: string;
-}
+export const PasswordMixin = <T extends ClassType>(BaseClass: T) => {
+	@InputType()
+	class PasswordInput extends BaseClass {
+		@Field()
+		@Min(5)
+		password: string;
+	}
+
+	return PasswordInput;
+};
