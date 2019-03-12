e0fdbf99d7ffe2cc89bb55887996ec6d730d03cf
Rob Harding <robbie.harding@hotmail.com>
2019-03-12 16:04:12 -0400

    Added another test that uses context

--- src/test-utils/gCall.ts
+++ src/test-utils/gCall.ts
@@ -7,15 +7,26 @@ interface Options {
   variableValues?: Maybe<{
     [key: string]: any;
   }>;
+  userId?: number;
 }
 
 let schema: GraphQLSchema;
 
-export const gCall = async ({ source, variableValues }: Options) => {
+export const gCall = async ({ source, variableValues, userId }: Options) => {
   if (!schema) schema = await createSchema();
   return graphql({
     schema: await createSchema(),
     source,
-    variableValues
+    variableValues,
+    contextValue: {
+      req: {
+        session: {
+          userId
+        }
+      },
+      res: {
+        clearCookie: jest.fn()
+      }
+    }
   });
 };
