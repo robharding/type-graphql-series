e5f4a396d49a6a0796341aef40a76eee93952f56
Rob Harding <robbie.harding@hotmail.com>
2019-03-20 13:49:35 -0400

    Added query complexity validation

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import { createConnection } from "typeorm";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import cors from "cors";
+import QueryComplexity, {
+  fieldConfigEstimator,
+  simpleEstimator
+} from "graphql-query-complexity";
+
 import { redis } from "./redis";
 import { createSchema } from "./utils/createSchema";
 
@@ -17,7 +22,22 @@ const main = async () => {
   const apolloServer = new ApolloServer({
     schema,
     formatError: formatArgumentValidationError,
-    context: ({ req, res }: any) => ({ req, res })
+    context: ({ req, res }: any) => ({ req, res }),
+    validationRules: [
+      QueryComplexity({
+        maximumComplexity: 8,
+        variables: {},
+        onComplete: (complexity: number) => {
+          console.log(`Query Complexity: ${complexity}`);
+        },
+        estimators: [
+          fieldConfigEstimator(),
+          simpleEstimator({
+            defaultComplexity: 1
+          })
+        ]
+      }) as any
+    ]
   });
 
   const app = Express();
